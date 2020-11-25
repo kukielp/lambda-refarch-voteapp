c3572d15283d0a6241d52564421fa322a191ea10
ANT\sicart <sicart@amazon.com>
2020-11-25 15:06:38 +1100

    Added ddb CDK and permission grant example

--- lib/a-stack.ts
+++ lib/a-stack.ts
@@ -1,18 +1,38 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
+
 import * as cdk from '@aws-cdk/core';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as lambda from '@aws-cdk/aws-lambda';
+
 
 export class AStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+
+    //dynamo tables
+    const VotesTable = new dynamodb.Table(this, 'VoteApp', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+    });
+   
+    const AggregatesTable = new dynamodb.Table(this, 'AggregatesTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
     });
 
-    const topic = new sns.Topic(this, 'ATopic');
+    
+    //sample lambda - feel free to delete
+    const handler = new lambda.Function(this, "WidgetHandler", {
+      runtime: lambda.Runtime.NODEJS_10_X, // So we can use async in widget.js
+      code: lambda.Code.asset("resources"),
+      handler: "widgets.main"
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    
+
+    //security for DDB
+    AggregatesTable.grantReadWriteData(handler);
+    VotesTable.grantReadWriteData(handler);
+
   }
 }
