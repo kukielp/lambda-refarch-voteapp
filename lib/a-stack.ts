22966a7d7d571d1ab8a7772eff5f98c8c39810ca
Paul Kukiel <kukiel@amazon.com>
2020-11-25 15:20:02 +1100

    Add SNS topic

--- lib/a-stack.ts
+++ lib/a-stack.ts
@@ -1,12 +1,22 @@
 import * as cdk from '@aws-cdk/core';
+import * as sns from '@aws-cdk/aws-sns';
+
 const pinpoint =  require("@aws-cdk/aws-pinpoint");
+
 
 export class AStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //Pinpoint Project
     const pinpointProject = new pinpoint.CfnApp(this, "vote4cdk", {
       name: "vote4cdk"
     });
+
+    //Pipoint SNS topic
+    const topic = new sns.Topic(this, 'Topic', {
+      displayName: 'Pinpoint Incomming subscription topic'
+    });
+
   }
 }
